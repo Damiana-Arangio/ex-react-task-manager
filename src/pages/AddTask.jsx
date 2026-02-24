df4dc997203b481f962dc1f5d873c9d9e4a09c7b
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-24 13:15:53 +0100

    feat: milestone 5 - creato form AddTask con validazione del titolo e gestione submit

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,13 +1,125 @@
 /* Pagina che consente di aggiungere un nuovo Task */
+import { useMemo, useRef, useState } from "react";
+
+// Set di caratteri validi per la validazione di title
+const symbols = `!@#$%^&*()-_=+[]{}|;:'\\",.<>?/~`;
 
 function AddTask() {
+
+    /**********
+        HOOK
+    ***********/
+    // Campi Controllati
+    const [title, setTitle] = useState("");
+
+    // Campi NON controllati
+    const descriptionRef = useRef();
+    const statusRef = useRef()
+
+    /*****************
+        VALIDAZIONI
+    ******************/
+    // Validazione titolo
+    const isTitleValid = useMemo (() => {
+        let isValid = true;
+
+        // Controllo se title è vuoto
+        if (title.trim().length === 0) {
+            isValid = false;
+        } else {
+            // Controllo se title contiene simboli vietati
+            const titleArray = title.split("");                     // Conversione stringa -> array
+            isValid = titleArray.every(carattereTitolo => (
+                !symbols.includes(carattereTitolo)
+            ))
+        }
+
+        return isValid;
+
+    }, [title]);
 
     /*************
         RENDER 
     *************/
     return(
-        <h1>AddTask</h1>
+        <form onSubmit={handleSubmit}>
+
+            {/* Titolo */}
+            <div>
+                <label htmlFor="title"> Titolo </label>
+                <input
+                    id="title"
+                    name="title"
+                    type="text"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+            </div>
+
+            {/* Validazione titolo */}
+            {
+                title && (
+                    <p>
+                        {!isTitleValid ? "Titolo non valido!" : ""}
+                    </p>
+                )
+            }
+
+
+            {/* Descrizione  */}
+            <div>
+                <label htmlFor="description"> Descrizione </label>
+                <textarea
+                    id="description"
+                    name="description"
+                    ref={descriptionRef}
+                />
+            </div>
+
+            
+            {/* Stato  */}
+            <div>
+                <label htmlFor="status"> Stato </label>
+                <select 
+                    id="status"
+                    name="status"
+                    ref={statusRef}
+                    defaultValue="to-do"
+                >
+                    <option value="to-do"> To do</option>
+                    <option value="doing"> Doing</option>
+                    <option value="done"> Done </option>
+                </select>
+            </div>
+
+            {/* Bottone per aggiungere il nuovo task */}
+            <button 
+                type="submit"
+                disabled={!isTitleValid}
+            > 
+                Aggiungi Task 
+            </button>
+                
+        </form>
     )
+
+    /**************
+        FUNZIONI
+    ***************/
+
+    // Funzione che gestisce il submit del form
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        // Creo nuovo task
+        const newTask = {
+            title: title,
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+
+        console.log("New Task: ", newTask);
+    }
 }
 
 export default AddTask;
