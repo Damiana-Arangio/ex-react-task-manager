fde2201f117a6b808b80c645577617d1853016bb
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-25 23:11:28 +0100

    feat: milestone 9 - creato componente Modal riutilizzabile con React Portal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,31 @@
+import { createPortal } from "react-dom";
+
+function Modal(props) {
+    
+    const { title, content, show, onClose, onConfirm, confirmText = "Conferma"} = props;
+    
+    
+    // Contenuto Modale
+    return show && createPortal (
+
+        <div className="overlay-modale">
+            <div className="modale">
+
+                {/* Contenuto principale */}
+                <h1> {title} </h1>
+                <p> {content} </p>
+
+                {/* Bottoni */}
+                <button onClick={onClose}> Annulla </button>
+                <button onClick={onConfirm}> {confirmText} </button>
+            </div>
+
+        </div>,
+
+
+        // Nodo del DOM in cui la modale viene montata
+        document.body
+    );
+}
+
+export default Modal;
