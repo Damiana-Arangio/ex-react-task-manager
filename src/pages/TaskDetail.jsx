2c56a43ccf59d577a51b5f3c3286f1feecc84ae7
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-25 16:03:20 +0100

    feat: milestone 7 - implementata pagina TaskDetail con recupero task da context e render dettagli

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,13 +1,43 @@
 // Componente che mostra i dettagli di un task
 import { useParams } from "react-router-dom";
+import { useTasksContext } from '../context/TasksContext'
 
 function TaskDetail() {
 
-    // Recupero id dall'url
-    const {id} = useParams();
+    // Destructuring
+    const { id } = useParams();
+    const numericId = parseInt(id);
 
+    const { tasks } = useTasksContext();
+
+    // Recupero task
+    const taskClicked = tasks.find(task => task.id === numericId);    
+    console.log("taskClicked: ", taskClicked);
+
+    if (!taskClicked) {
+        return <h2>Task non trovata</h2>
+    }
+    /************
+        RENDER
+    *************/
     return(
-        <h1>id del tak: {id }</h1>
+        <>
+            <article>
+                <h1> {taskClicked.title} </h1>
+                <p> {taskClicked.description}</p>
+                <p>
+                    <strong>Stato:</strong> {taskClicked.status}
+                </p>
+                <p>
+                    <strong>Data di creazione:</strong>{" "}
+                    <time dateTime={taskClicked.createdAt}>
+                        {new Date(taskClicked.createdAt).toLocaleDateString()}
+                    </time>
+                </p>
+            </article>
+
+            <button onClick={()=> console.log("Elimino task", numericId)}> Elimina Task</button>
+        </>
     )
 }
 
