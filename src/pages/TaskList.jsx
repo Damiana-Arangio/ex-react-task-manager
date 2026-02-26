8ac934f41ef7b78a16f6422cde09c50c79e01180
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-26 23:24:37 +0100

    feat: milestone 12 - aggiunto filtro di ricerca case insensitive  ed integrato con ordinamento

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,99 @@
 /* Pagina che mostra l'elenco dei Task */
 import { useTasksContext } from '../context/TasksContext'
 import TaskRow from '../components/TaskRow';
+import { useMemo, useState } from 'react';
 
 function TaskList() {
     const { tasks } = useTasksContext();
+
+    /*************
+        HOOK 
+    *************/
+    const [sortBy, setSortBy] = useState("createdAt");          // criterio di ordinamento (title, status, createdAt).
+    const [sortOrder, setSortOrder] = useState(1);              // direzione (1 per crescente, -1 per decrescente)
+    const [searchQuery, setSearchQuery] = useState("")          // memorizzare il valore dell'input di ricerca
+    
+    // Task ordinati
+    const filteredAndSortedTasks = useMemo(() => {
+        
+        // Filtro di ricerca per titolo task
+        const filteredTasks = tasks.filter(task =>
+            task.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        // Copia array filtrato per ordinamento
+        const tasksCopy = [...filteredTasks];
+
+        // Ordinamento per titolo
+        if (sortBy === "title") {
+            tasksCopy.sort( (a,b) => {
+
+                // Titolo crescente
+                if (sortOrder === 1) {
+                    return a.title.localeCompare(b.title);
+                }
+
+                // Titolo decrescente
+                else{
+                    return b.title.localeCompare(a.title);
+                }
+                
+            } )
+        }
+
+        // Ordinamento per stato (numerico)
+        if (sortBy === "status") {
+
+            const statusOrder = {
+                "To do": 0,
+                "Doing": 1,
+                "Done": 2
+            };
+
+            tasksCopy.sort((a, b) => {
+
+                // Trasformo gli status in numeri
+                const valueA = statusOrder[a.status];
+                const valueB = statusOrder[b.status];
+
+                // Stato crescente
+                if (sortOrder === 1) {
+                    return valueA - valueB;
+                }
+
+                // Stato decrescente
+                else {
+                    return valueB - valueA;
+                }
+            });
+        }
+
+        // Ordinamento per data
+        if (sortBy === "createdAt") {
+
+            tasksCopy.sort((a, b) => {
+
+                // Trasformo le date in numeri (millisecondi trascorsi dal 1970)
+                const dateA = new Date(a.createdAt).getTime();
+                const dateB = new Date(b.createdAt).getTime();
+
+                // Data crescente
+                if (sortOrder === 1) {
+                    return dateA - dateB;
+                } 
+
+                // Data crescente
+                else {
+                    return dateB - dateA;
+                }
+
+            });
+        }
+
+        return tasksCopy;
+
+    }, [tasks, sortBy, sortOrder, searchQuery]);
+
 
     /*************
         RENDER 
@@ -11,27 +101,53 @@ function TaskList() {
     return (
 
         <>
+
+            {/* Barra di ricerca */}
+            <input 
+                type="text"
+                placeholder='Cerca un task...'
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+            />
+
             {/* Tabella task */ }
             <table>
 
                 {/* Header */}
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Stato</th>
-                        <th>Data di creazione</th>
+                        <th onClick={() => handleSort("title")}>Nome</th>
+                        <th onClick={() => handleSort("status")}>Stato</th>
+                        <th onClick={() => handleSort("createdAt")}> Data di creazione </th>
                     </tr>
                 </thead>
 
                 {/* Body */}
                 <tbody>
-                    {tasks.map(task => (
+                    {filteredAndSortedTasks.map(task => (
                         <TaskRow key={task.id} task={task} />
                     ))}
                 </tbody>
             </table>
         </>
     )
+
+    /*************
+        FUNZIONI 
+    *************/
+    function handleSort(column) {
+
+        // Se la colonna è già selezionata → inverto ordine (decrescente)
+        if (sortBy === column) {
+            setSortOrder(currNum => currNum * -1);
+        } 
+
+            // Se clicco una colonna diversa → imposto sortBy sulla nuova colonna e resetto l'ordine (crescente)
+        else {
+            setSortBy(column);
+            setSortOrder(1);
+        }
+    }
 }
 
 export default TaskList;
